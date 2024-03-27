606144bb77378c7a3cf4c4fae7b4bba9b731e04e
fwldom <m989mahdi@gmail.com>
2024-03-27 19:27:26 +0330

    End Calculator Base Programming

---
+++ index.js
@@ -0,0 +1,23 @@
+function append(text) {
+    if (document.getElementById("display").value == "Syntax Error") {
+        document.getElementById("display").value = "";
+    }
+    document.getElementById("display").value += text;
+    var inputField = document.getElementById('display');
+    var length = inputField.value.length;
+    inputField.style.fontSize = (length > 8 ? (35 / (length / 100)) + '%' : '4rem');
+}
+function calculate() {
+    try {
+        document.getElementById("display").value = eval(document.getElementById("display").value);
+    }
+    catch {
+        document.getElementById("display").value = "Syntax Error"
+    }
+    var inputField = document.getElementById('display');
+    var length = inputField.value.length;
+    inputField.style.fontSize = (length > 8 ? (35 / (length / 100)) + '%' : '4rem');
+}
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
